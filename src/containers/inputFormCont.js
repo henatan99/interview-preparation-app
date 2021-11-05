ca9521ae4fb61e874133fd077cc6c834f4f0f210
Henok Mekonnen <henatan99@yahoo.com>
2021-11-05 18:39:32 +0300

    Add input form container component

---
+++ src/containers/inputFormCont.js
@@ -0,0 +1,6 @@
+import InputForm from "../components/inputForm";
+
+export const InputFormCont = () => {
+    const inputFormCont = document.createElement('div');
+    inputFormCont.appendChild(InputForm());
+}
